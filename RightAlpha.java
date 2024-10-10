b591d310d64d35d25d11c979bac80f1764373c5e
komal-nariyal <komalnariyal@gmail.com>
2024-10-10 11:15:53 +0530

    First Commit

---
+++ RightAlpha.java
@@ -0,0 +1,11 @@
+public class RightAlpha{
+    public static void main(String[]args){
+        int a=65;
+for(int i=0; i<4; i++){
+    for(int j=0; j<=i; j++){
+        System.out.print((char)(a+i)+" ");
+    }
+ System.out.println();
+}
+    }
+}
---
+++ SandglassStar.java
@@ -0,0 +1,22 @@
+public class SandglassStar {
+    public static void main (String []args){
+        for(int i=1; i<5; i++){
+            for(int k=1; k<i; k++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=5-i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+            for(int i=1; i<5; i++){
+                for(int k =3; k>=i; k--){
+                    System.out.print(" ");
+                }
+                for(int j=1; j<=i; j++){
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
+        }
+    }
