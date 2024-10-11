1fd08db894e22172df2c6e8b8ebd9d4aa158c802
komal-nariyal <komalnariyal@gmail.com>
2024-10-11 10:10:56 +0530

    2 files added

---
+++ SquareParallel.java
@@ -0,0 +1,13 @@
+public class SquareParallel{
+    public static void main(String[]args){
+        for(int i=1; i<5; i++){
+            for(int j=1; j<=5; j++){
+                if(j==1|| j==5)
+                System.out.print("*");
+                else
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
